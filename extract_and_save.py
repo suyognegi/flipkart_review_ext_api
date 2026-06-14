a2ccd9296ab235fe5bd3071a6f9c370f678b933b
suyognegi <suyognegi1@gmail.com>
2026-06-14 21:20:01 +0530

    Update extract_and_save.py

--- extract_and_save.py
+++ extract_and_save.py
@@ -1,1 +1,16 @@
 
+import asyncio
+from review_processor import review_processor
+
+
+async def start():
+    processor = review_processor(
+        product_name="smartphone",
+        review_limit=90
+    )
+
+    await processor.init_urls()
+    processor.run()
+
+
+asyncio.run(start())
