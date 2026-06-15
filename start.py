164e8f7aa5ef1a157da4ea881d02f1ef622e360d
suyognegi <suyognegi1@gmail.com>
2026-06-15 11:45:04 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,1 +1,19 @@
 
+
+
+import asyncio
+from review_processor import review_processor
+
+
+async def start():
+    processor = review_processor(
+        product_name="smartphone",
+        review_limit=90,
+        page=8
+    )
+
+    await processor.init_urls()
+    processor.run()
+
+
+asyncio.run(start())
